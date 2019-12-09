65efa668cab3a4032a77869927466e12ba596918
Alex Silva <alexsouzasilvax@gmail.com>
2019-12-08 23:51:15 -0300

    2.3 16:43

--- src/main/java/com/algaworks/cobranca/Controller/TituloController.java
+++ src/main/java/com/algaworks/cobranca/Controller/TituloController.java
@@ -2,12 +2,22 @@ package com.algaworks.cobranca.Controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.algaworks.cobranca.model.Titulo;
 
 @Controller
+@RequestMapping("/titulos/novo")
 public class TituloController {
-	
+
 	@RequestMapping("/titulos/novo")
 	public String novo() {
+		return "CadastroTitulo";
+	}
+
+	@RequestMapping(method = RequestMethod.POST)
+	public String salvar(Titulo titulo) {
+		System.out.println("--> " + titulo.getDescricao() + " <--");
 		return "CadastroTitulo";
 	}
 }
